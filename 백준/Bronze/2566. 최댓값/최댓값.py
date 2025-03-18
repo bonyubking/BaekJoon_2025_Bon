836592077b99b8106caf77652e488f0041315169
Gu_BonYub <piusku2@gmail.com>
2025-03-18 22:54:10 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,18 +1,23 @@
 numbers = []
-max = 0
+maxval = 0
 col = 1
 row = 1
 
 for i in range (9):
-    numbers.append(list(map(int,input().split())))
+    lst = list(map(int,input().split()))
+    if max(lst) > maxval:
+        col = i+1
+        row = lst.index(max(lst))+1
+        maxval = max(lst) 
+#     numbers.append(lst)
     
 
-for i in range(9):
-    for j in range(9):
-        if numbers[i][j] > max:
-            max = numbers[i][j]
-            col = i+1;
-            row = j+1;    
+# for i in range(9):
+#     for j in range(9):
+#         if numbers[i][j] > maxval:
+#             maxval = numbers[i][j]
+#             col = i+1;
+#             row = j+1;    
     
-print(max)
+print(maxval)
 print(col, row) 
