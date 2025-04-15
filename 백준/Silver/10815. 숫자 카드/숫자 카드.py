20d17bd786d404d965483614233a6ad0ebdd2648
Gu_BonYub <piusku2@gmail.com>
2025-04-15 22:20:08 +0900

    [Silver V] Title: 숫자 카드, Time: 480 ms, Memory: 118308 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -5,16 +5,14 @@ Nset = set(map(int,input().split()))
 M = int(input())
 
 Mlist = list(map(int,input().split()))
-Mset = set(Mlist)
 
 Keyset = set()
 
-
-Keyset = Nset & Mset
+Nset 
         
             
 for i in range(len(Mlist)):
-    if Mlist[i] in Keyset:
+    if Mlist[i] in Nset:
         Mlist[i] = 1
     else:
         Mlist[i] = 0
