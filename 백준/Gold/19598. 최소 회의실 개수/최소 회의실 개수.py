7c9d2d945caa9ac99d1afcae75c45814ef9ea112
Gu_BonYub <piusku2@gmail.com>
2025-05-27 12:59:34 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 2904 ms, Memory: 54724 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -0,0 +1,25 @@
+import heapq
+
+N = int(input())
+
+lst = []
+cnt = 1
+fin = [0]
+
+for i in range(N):
+    lst.append(list(map(int,input().split())))
+    
+lst.sort()
+
+
+
+heap = []
+for s, e in lst:
+    if heap and heap[0] <= s:
+        heapq.heapreplace(heap, e)   
+    else:
+        heapq.heappush(heap, e) 
+
+print(len(heap)) 
+    
+
