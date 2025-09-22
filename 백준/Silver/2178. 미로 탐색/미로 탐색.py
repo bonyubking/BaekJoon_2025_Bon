a7f734128fee1f6c99dc15007d01dc8509a93f96
Gu_BonYub <piusku2@gmail.com>
2025-09-23 03:47:57 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 114360 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -8,27 +8,37 @@ dc = [0, 1, 0, -1]
 
 graph = [] 
 
-
-
 for _ in range(N):
     key = list(map(int, input().strip()))
     graph.append(key)
+    
+visited = [[False]*M for _ in range(N)]
+    
 
 Q = deque()
 Q.append((0,0))
+visited[0][0] = True
+ans = 1
 
 while Q:
-    i,j = Q.popleft()
+    
+    for _ in range(len(Q)):
+        i,j = Q.popleft()
+        
+        if (i,j) == (N-1, M-1):
+            print(ans)
+            exit()
 
-    for k in range (4):
-        di = dr[k] + i
-        dj = dc[k] + j
-                
-        if 0 <= di < N and 0 <= dj < M and graph[di][dj] == 1:
-                
-            graph[di][dj] = graph[i][j] + 1
-            Q.append([di, dj])
+        for k in range (4):
+            di = dr[k] + i
+            dj = dc[k] + j
                     
-
-
-print(graph[N-1][M-1])
+            if 0 <= di < N and 0 <= dj < M and graph[di][dj] == 1 and not visited[di][dj]:
+                    
+                visited[di][dj] = True
+                Q.append([di, dj])
+    
+    ans += 1
+    
+    
+print(ans)
