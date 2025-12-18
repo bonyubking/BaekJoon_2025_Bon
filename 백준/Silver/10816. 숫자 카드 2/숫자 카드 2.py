626164d920e8c782fb4e0a3862e6c77d92821a2f
Gu_BonYub <piusku2@gmail.com>
2025-12-18 02:41:44 -0800

    [Silver IV] Title: 숫자 카드 2, Time: 480 ms, Memory: 260500 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,38 +1,22 @@
 N = int(input())
 
-numlst = sorted(list(map(int,input().split())))
+numlst = list(map(int,input().split()))
 
 M = int(input())
 
 tarlst = list(map(int,input().split()))
 
-numcnt = {}
+counts = {}
 
-for n in numlst:
-    if n in numcnt:
-        numcnt[n] += 1
+for key in numlst:
+    if key in counts:
+        counts[key] += 1
     else:
-        numcnt[n] = 1
+        counts[key] = 1
         
-def binsearch(array, target, start, end):
-    
-    while end >= start:
-        
-        mid = (start + end) // 2
-        
-        if target == array[mid]:
-            
-            return numcnt[array[mid]]
-    
-        elif target < array[mid]:
-            
-            end = mid - 1
-        
-        else:
-            
-            start = mid + 1
-            
-    return 0;
 
-for i in tarlst:
-    print(binsearch(numlst, i, 0, N-1), end= ' ')
+for key2 in tarlst:
+    if key2 not in counts:
+        print('0', end =' ')
+    else:
+        print(counts[key2], end=' ')
