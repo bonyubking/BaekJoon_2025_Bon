82af89d694b802c78c6121300aa57432f7341f8e
Gu_BonYub <piusku2@gmail.com>
2026-01-11 21:13:54 -0800

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 60 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -1,30 +1,41 @@
-N, M = map(int,input().split())
+from collections import deque
 
-friends = [[1e9] * (N+1)  for _ in range (N+1)]
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+
+def bfs(start):
+    visited = [False] * (N+1)
+    dist = [0] * (N+1)
+    
+    q = deque([start])
+    visited[start] = True
+    dist[start] = 0
+    
+    while q:
+        cur = q.popleft()
+        for nxt in graph[cur]:
+            if not visited[nxt]:
+                visited[nxt] = True
+                dist[nxt] = dist[cur] + 1
+                q.append(nxt)
+    
+    return sum(dist[1:])
+
+
 target = 1e9
 
-for i in range(1, N+1):
-    friends[i][i] = 0
-    
-for _ in range (M):
-    a,b = map(int,input().split())
-    friends[a][b] = 1
-    friends[b][a] = 1
-
-for k in range(1, N+1):
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            friends[i][j] = min(friends[i][j], friends[i][k] + friends[k][j])
-            
 for i in range (1, N+1):
-    temp = 0
-    for j in range (1, N+1):
-        temp += friends[i][j]
-    
-    if temp < target:
-        target = temp
+    total = bfs(i)
+    if total < target:
+        target = total
         ans = i
         
 print(ans)
-
-    
+        
