8b36965ac90f148f9df8eea8a8e0cfd19a399ca1
Gu_BonYub <piusku2@gmail.com>
2026-03-05 13:34:03 +0900

    [Gold III] Title: 다리 만들기, Time: 108 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -15,12 +15,16 @@ dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 landnum = 0
 
+templst = []
+
 for i in range(N):
     for j in range(N):
         if maps[i][j] == 1 and visited[i][j] == 0:
+            dist[i][j] = 0
             landnum+=1
             visited[i][j] = landnum
             q=deque([(i,j)])
+            templst.append([i,j])
             
             while q:
                 x, y = q.popleft()
@@ -30,24 +34,20 @@ for i in range(N):
                     if 0<=nx<N and 0<=ny<N and visited[nx][ny]==0 and maps[nx][ny] == 1:
                         visited[nx][ny] = landnum
                         q.append([nx,ny])
+                        templst.append([nx,ny])
+                        dist[nx][ny] = 0
 
 ans = 1e9
 
-for s in range(1, landnum + 1):
-
-    q = deque()
-
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] == s:
-                q.append((i, j))
-                dist[i][j] = 0
-                
-    while q:
-        x, y = q.popleft()
+for a,b in templst:
+    
+    q2 = deque([(a,b)]) 
+      
+    while q2:
+        x, y = q2.popleft()
         
         if dist[x][y]+1 > ans:
-            break
+            continue
         
         for h in range(4):
             nx = x+dx[h]
@@ -55,14 +55,14 @@ for s in range(1, landnum + 1):
             if 0 <= nx < N and 0 <= ny < N:
                 if maps[nx][ny] == 0 and dist[nx][ny] > dist[x][y]+1:
                     dist[nx][ny] = dist[x][y] + 1
-                    q.append((nx, ny))
+                    q2.append((nx, ny))
 
-                elif visited[nx][ny] != 0 and visited[nx][ny] != s:
+                elif visited[nx][ny] != 0 and visited[nx][ny] != visited[a][b]:
                     ans = min(ans, dist[x][y])
-                    q.clear()
                     break;
 
+print(ans)
 
-print(ans)                                        
+                                  
 
         
